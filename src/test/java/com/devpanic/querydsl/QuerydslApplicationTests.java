ee7355a50861f25aca6294ee2e638b92d0e16ee1
devpanic <yeon.dev.8@gmail.com>
2024-09-21 17:00:41 +0900

    initial commit

---
+++ src/test/java/com/devpanic/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpanic.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
