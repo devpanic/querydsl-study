87af23a5271c299e8241a18d8fe39eee17088bc5
devpanic <yeon.dev.8@gmail.com>
2024-09-21 19:41:13 +0900

    feat: Q Class 생성

--- src/test/java/com/devpanic/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/devpanic/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,33 @@
 package com.devpanic.querydsl;
 
+import com.devpanic.querydsl.entity.Hello;
+import com.devpanic.querydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+    @Autowired
+    EntityManager em;
 
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = new QHello("h");
+
+        Hello result = query.selectFrom(qHello).fetchOne();
+
+        Assertions.assertThat(result).isEqualTo(hello);
+        Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
 }
